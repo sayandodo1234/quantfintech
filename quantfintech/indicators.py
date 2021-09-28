6ea785d3471a9c5e482585588c82404f6f2820d9
Sayan De Bhowmick <debhowmick.sayan@gmail.com>
2021-09-28 23:21:30 +0530

    Updated indicators.py

--- quantfintech/indicators.py
+++ quantfintech/indicators.py
@@ -18,16 +18,21 @@ from . import assist as at
 class Security:
     
 
-    def __init__(self, security_df,index_date = True):
+    def __init__(self, security_df):
         self.ohlc_df = security_df.copy(deep=True)
-        if index_date == True:
-            self.ohlc_cols = ['open', 'high', 'low', 'close','volume']
-        else:
-            self.ohlc_cols = ['date','open', 'high', 'low', 'close','volume']
+        self.ohlc_cols = ['open', 'high', 'low', 'close','volume']
+        self.process()
+            
+
+    def process(self):
+        self.ohlc_df.rename(columns = {"Open":"open", "Close":"close","Volume":"volume","High":"high","Low":"low"},inplace = True)
         if pd.Series(self.ohlc_cols).isin(self.ohlc_df.columns).all():
             self.ohlc_df = self.ohlc_df[self.ohlc_cols]
         else:
+            
             commomCols_set = set(self.ohlc_cols) & set(self.ohlc_df.columns)
+            #commonCols_list_temp = list(commomCols_set)
+            #commonCols_list = [cols.lower() for cols in commonCols_list_temp]
             missingCols_list = list(set(self.ohlc_cols) - commomCols_set)
             print("Missing columns : {0}".format(missingCols_list))
             sys.exit("Terminating")
