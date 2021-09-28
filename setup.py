669917d6070111ad54be4c1a6eea942ce2a3bed4
Sayan De Bhowmick <debhowmick.sayan@gmail.com>
2021-09-28 21:05:06 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
   author = 'Sayan De Bhowmick',                   
   author_email = 'debhowmick.sayan@gmail.com',      
   url = 'https://github.com/sayandodo1234/quantfintech',   
-  download_url = 'https://github.com/sayandodo1234/quantfintech/archive/refs/tags/v_01.2.zip',    
+  download_url = 'https://github.com/sayandodo1234/quantfintech/archive/refs/tags/v_01.3.zip',    
   keywords = ['Technical Indicators', 'Quant Analysis', 'Algo trading'],
   install_requires=[           
           'pandas',
